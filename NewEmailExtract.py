d4535f5e4a16ed5bcdf1433b6f9163f8dc8b55cf
Avinash Singh <avi104@yahoo.co.in>
2025-10-16 15:33:36 +0530

    Updates

--- NewEmailExtract.py
+++ NewEmailExtract.py
@@ -19,7 +19,7 @@ from sqlalchemy import create_engine, text
 from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
 import docx
 from bs4 import BeautifulSoup
-from html2docx import HtmlToDocx
+from html2docx import Html2Docx
 
 # --- PAGE CONFIGURATION ---
 st.set_page_config(
